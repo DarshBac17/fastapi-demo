8e5de09fd705b78e485988d92b88efd8a8f7e849
DarshBac17 <darsh.shah@bacancy.com>
2025-05-09 17:12:34 +0530

    updated user table

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -10,7 +10,7 @@ class User(Base):
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
-    # age = Column(Integer,nullable=True)
+    age = Column(Integer,nullable=True)
     # hobby = Column(String,nullable=True)
     # phone = Column(String,nullable=True)
 
